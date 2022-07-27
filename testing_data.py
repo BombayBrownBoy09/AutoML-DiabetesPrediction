eeafa58ea53c896bdbc0b70983426e0b62052b01
Bhargav Shetgaonkar <67541365+BombayBrownBoy09@users.noreply.github.com>
2022-07-27 12:22:39 -0400

    Update testing_data.py

--- testing_data.py
+++ testing_data.py
@@ -1,20 +1,13 @@
 import pandas as pd
 from deepchecks.tabular import Dataset
-from deepchecks.tabular.checks import DataDuplicates
-from deepchecks.tabular.checks import ConflictingLabels
+from deepchecks.tabular.suites import data_integrity
 
-
-def test_data():
-    # Load dataset
-    df = pd.read_csv("data/spam.csv")
-    dataset = Dataset(
-        df, cat_features=["text"], label="label", features=["text"]
-    )
-    # References:
-    # https://docs.deepchecks.com/stable/checks_gallery/tabular/data_integrity/plot_data_duplicates.html
-    # https://docs.deepchecks.com/stable/checks_gallery/tabular/data_integrity/plot_conflicting_labels.html
-    # Run the checks
-    duplicates = DataDuplicates().run(dataset).display
-    conflicting_labels = ConflictingLabels().run(dataset).display
-    assert len(duplicates) == 0
-    assert len(conflicting_labels) == 0
+def test_data_integrity():
+    # The label can be passed as a column name or a separate pd.Series / pd.DataFrame
+    df = pd.read_csv("data/diabetes.csv")
+    ds = Dataset(df, label="Outcome")
+    # Run Suite
+    integ_suite = data_integrity().remove(0)
+    suite_result = integ_suite.run(ds)
+    suite_result.save_as_html(file="deepchecks.html")
+    assert len(suite_result.get_not_passed_checks()) == 0
